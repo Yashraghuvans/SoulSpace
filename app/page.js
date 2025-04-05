05111292441e1c9948d0357492f02b6cb12e803c
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-05 14:54:42 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
+import Main from './Components/Main'
 
 const page = () => {
   return (
     <>
       <div>
         <NavBar/>
+        <Main/>
       </div>
     </>
   )
