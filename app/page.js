d5506b674ab299cd94bd83b079b55a8ed5a118ea
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-05 19:26:58 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
 import Main from './Components/Main'
+import Services from './Components/Services'
 
 const page = () => {
   return (
     <>
       <div className='bg-black/70'>
-        <NavBar/>
-        <Main/>
+        <NavBar />
+        <div className='h-full'>
+          <Main />
+        </div>
+        <Services/>
       </div>
     </>
   )
