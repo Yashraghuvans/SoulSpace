77e2e28e0d5c51763c5f0cf79f15418fb9c7eebf
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-06 07:57:48 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -18,10 +18,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={geistSans.variable}>
+      <body className={`${geistSans.className} font-sans antialiased`}>
         {children}
       </body>
     </html>
